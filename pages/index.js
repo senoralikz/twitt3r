36bee7ff2dbfd00b9b02d2d7634989c3a98b7616
senoralikz <alexjm825@gmail.com>
2023-03-22 21:15:24 -0400

    added console log to see if tweets gets fetched in production

--- pages/index.js
+++ pages/index.js
@@ -6,31 +6,31 @@ import { fetchTweets } from "../utils/fetchTweets.js";
 import { Toaster } from "react-hot-toast";
 
 export default function Home({ tweets }) {
-  // console.log(tweets);
-  return (
-    <div className="lg:max-w-6xl mx-auto max-h-screen overflow-hidden">
-      <Head>
-        <title>twitt3r</title>
-        <link rel="icon" href="/twitt3r-icon.png" />
-      </Head>
+    console.log(tweets);
+    return (
+        <div className="lg:max-w-6xl mx-auto max-h-screen overflow-hidden">
+            <Head>
+                <title>twitt3r</title>
+                <link rel="icon" href="/twitt3r-icon.png" />
+            </Head>
 
-      <Toaster />
+            <Toaster />
 
-      <main className="grid grid-cols-9">
-        <Sidebar />
-        <Feed tweets={tweets} />
-        <Widgets />
-      </main>
-    </div>
-  );
+            <main className="grid grid-cols-9">
+                <Sidebar />
+                <Feed tweets={tweets} />
+                <Widgets />
+            </main>
+        </div>
+    );
 }
 
 export const getServerSideProps = async (context) => {
-  const tweets = await fetchTweets();
+    const tweets = await fetchTweets();
 
-  return {
-    props: {
-      tweets,
-    },
-  };
+    return {
+        props: {
+            tweets,
+        },
+    };
 };
